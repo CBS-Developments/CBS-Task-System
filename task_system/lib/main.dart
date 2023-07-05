9d07de19cf429124bcc0f8b3bc92190ae79263f0
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-07-05 13:53:50 +0530

    Create main Task

--- task_system/lib/main.dart
+++ task_system/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_system/pages/createMainTask.dart';
 import 'package:task_system/pages/login_page.dart';
 
 void main() {
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: MainTask(),
     );
   }
 }
