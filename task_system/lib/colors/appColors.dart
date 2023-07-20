0cad5f9c9d9b2fcff0b77e328b5b85f3f6043170
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-07-20 10:51:47 +0530

    Create appColors.dart

---
+++ task_system/lib/colors/appColors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColor{
+  static  Color teal=const Color(0xFF077777);
+  static  Color lightBlue = const Color(0xFFF0F2FE);
+  static  Color blueGreen = const Color(0xFFB6DDDF);
+
+}
