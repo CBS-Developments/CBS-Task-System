694c60b160ab96b6a0b42b30d760d20185e42716
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-07-22 21:38:23 +0530

    Create createSubTask.dart

---
+++ task_system/lib/pages/tasks/createSubTask.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SubTask extends StatefulWidget {
+  const SubTask({Key? key}) : super(key: key);
+
+  @override
+  State<SubTask> createState() => _SubTaskState();
+}
+
+class _SubTaskState extends State<SubTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
