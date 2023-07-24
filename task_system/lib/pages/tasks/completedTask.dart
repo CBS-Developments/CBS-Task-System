6f5301d15294083074a7152a54be9ea842a9a0e7
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-07-24 17:18:22 +0530

    Create completedTask.dart

---
+++ task_system/lib/pages/tasks/completedTask.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../../colors/appColors.dart';
+import '../../components/appBar.dart';
+import '../../components/navigate_text.dart';
+import '../../components/search_box.dart';
+import '../../drawers/mainTaskDrawer.dart';
+import '../../sizes/create_MainTask_Sizes.dart';
+import '../../sizes/dashboard_sizes.dart';
+import '../../tabs/task_tab.dart';
+
+class CompletedTask extends StatefulWidget {
+  const CompletedTask({Key? key}) : super(key: key);
+
+  @override
+  State<CompletedTask> createState() => _CompletedTaskState();
+}
+
+class _CompletedTaskState extends State<CompletedTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: myAppBar(context),
+        body: Row(
+          children: [
+            MainTaskDrawer(), // Call myDrawer as a function and pass the context
+
+            Container(
+              width: rightSideWidth(context),
+              height: getPageHeight(context),
+              // color: Colors.blueAccent,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: smallBoxHeight(context),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      NavigateText(
+                        firstText: 'Task Dashboard/',
+                        secText: 'Main Task/',
+                        thirdText: 'Create Main Task',
+                      ),
+                      SearchBox()
+                    ],
+                  ),
+                  SizedBox(
+                    height: smallBoxHeight(context),
+                  ),
+                  TaskTabBar(
+                    firstButtonColor: Colors.black,
+                    secButtonColor: Colors.black,
+                    thirdButtonColor: AppColor.tabBlueSelect,
+                    fourthButtonColor: Colors.black,
+                    fivethButtonColor: Colors.black,
+                  ),
+                  SizedBox(
+                    height: smallBoxHeight(context),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
