a3644727b32bf2b4b7f8b70188c9b29fab42573b
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-07-28 11:00:02 +0530

    logout button

--- task_system/lib/components/appBar.dart
+++ task_system/lib/components/appBar.dart
@@ -32,7 +32,7 @@ PreferredSizeWidget myAppBar(BuildContext context) {
           },
           icon: Icon(
             Icons.logout_outlined,
-            color: Colors.white,
+            color: Colors.grey,
           ),
           tooltip: 'Logout',
         ),
@@ -41,55 +41,4 @@ PreferredSizeWidget myAppBar(BuildContext context) {
   );
 }
 
-// var myDrawer = (BuildContext context) {
-//   final double fontSize = sideBarFont(context);
-//
-//
-//   return Drawer(
-//     width: drawerWidth(context),
-//     backgroundColor: Colors.grey[300],
-//     child: Column(children: [
-//       ListTile(
-//         leading: Icon(
-//           Icons.home,
-//           color: Colors.black54,),
-//         title: Text(
-//           'D A S H B O A R D',
-//           style: TextStyle(
-//             fontSize: fontSize,
-//           ),),
-//       ),
-//       ListTile(
-//         leading: Icon(
-//         Icons.settings,
-//         color: Colors.black54,),
-//         title: Text(
-//             'T A S K S',
-//           style: TextStyle(
-//             fontSize: fontSize,
-//           ),),
-//       ),
-//       ListTile(
-//         leading: Icon(
-//           Icons.person,
-//           color: Colors.black54,),
-//         title: Text(
-//             'P R O F I L E',
-//           style: TextStyle(
-//             fontSize: fontSize,
-//           ),),
-//       ),
-//       ListTile(
-//         leading: Icon(
-//           Icons.message_rounded,
-//           color: Colors.black54,),
-//         title: Text(
-//             'C H A T',
-//           style: TextStyle(
-//             fontSize: fontSize,
-//           ),),
-//       ),
-//     ]),
-//   );
-//   };
 
