ebf0aebf5180e921f196fb4ce156530f4684c6d7
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-08-05 15:50:46 +0530

    Create taskBox.dart

---
+++ task_system/lib/components/taskBox.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TaskBoxContainer extends StatelessWidget {
+  const TaskBoxContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
